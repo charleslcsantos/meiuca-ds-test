b1c9a9bc66af6ae2fb8b119aea9ecf7fe0446eee
Charles Lennon <charleslcsantos@gmail.com>
2023-12-05 16:32:15 -0300

    chore: adicionadod teste

--- src/components/card-content/CardContent.spec.tsx
+++ src/components/card-content/CardContent.spec.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CardContent from "./CardContent";
@@ -20,15 +19,13 @@ describe("CardContent Component", () => {
     expect(screen.getByText("Test Heading")).toBeInTheDocument();
     expect(screen.getByText("Test Subtitle")).toBeInTheDocument();
     expect(screen.getByText("Test Paragraph")).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Read More" })
-    ).toBeInTheDocument();
+    expect(screen.getByText("Read More")).toBeInTheDocument();
   });
 
   it("should render button with correct href and target", () => {
     render(<CardContent {...props} />);
 
-    const button = screen.getByRole("button", { name: "Read More" });
+    const button = screen.getByText("Read More");
     expect(button).toHaveAttribute("href", "https://example.com");
     expect(button).toHaveAttribute("target", "_blank");
   });
